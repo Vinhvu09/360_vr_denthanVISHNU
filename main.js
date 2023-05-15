4f89fbeacf18a140940fdc1a4a9fcebdf6d8345f
CNS29 <canhnguyena2@gmail.com>
2023-05-15 15:09:45 +0700

    update code

--- main.js
+++ main.js
@@ -1,49 +1,55 @@
-const data = Array(20).fill(0).map((x, idx) => {
-  return `images/khu den tho- Setup 0${idx + 1 > 9 ? idx + 1 : `0${idx + 1}`}.jpg`;
-});
-console.log(data);
+const data = Array(43)
+  .fill(0)
+  .map((x, idx) => {
+    return `images/khu den tho- Setup 0${
+      idx + 1 > 9 ? idx + 1 : `0${idx + 1}`
+    }.jpg`;
+  });
 
 let index = 0;
 const imageContainer = document.querySelector(".image-container");
-const btnNext = document.querySelector(".long-arrow-right");
-const btnPrev = document.querySelector(".long-arrow-left");
-
+const btnNext = document.querySelector(".triangle-top");
+const btnPrev = document.querySelector(".triangle-bottom");
 
 const viewer = new PANOLENS.Viewer({
   container: imageContainer,
   autoRotate: true,
   autoRotateSpeed: 0.3,
   controlBar: false,
-
 });
+let timer;
+
 load();
-btnPrev.classList.add("hide");
+
 btnNext.addEventListener("click", function () {
-  btnPrev.classList.remove("hide");
-  if (index >= data.length - 1) {
-    index = 0;
-  }
-  else {
-    index++;
-  }
+  index = index >= data.length - 1 ? 0 : ++index;
   load();
 });
+
 btnPrev.addEventListener("click", function () {
-  if (index === 0) {
-    this.classList.add("hide");
-    index = 0;
-  } else {
-    index--;
-  }
+  index = index === 0 ? 0 : --index;
   load();
 });
 
 function load() {
+  if (index === 0) {
+    btnPrev.classList.add("hide");
+  } else {
+    btnPrev.classList.remove("hide");
+  }
+
+  if (index === data.length - 1) {
+    btnNext.classList.add("hide");
+  } else {
+    btnNext.classList.remove("hide");
+  }
+
+  if (timer) {
+    clearTimeout(timer);
+  }
   const panoramaImage = new PANOLENS.ImagePanorama(data[index]);
-  viewer.dispose();
-  viewer.add(panoramaImage);
+  timer = setTimeout(() => {
+    viewer.dispose();
+    viewer.add(panoramaImage);
+  }, 100);
 }
-
-
-
-
